704f9c3f5d272889ce08c085b0179cd25fdfcabb
Zku11y <craneflam159@gmail.com>
2025-05-16 23:13:37 +0100

    Z(main:09) diffentiates priorities between syntax error and open quotes

--- parse/filter.c
+++ parse/filter.c
@@ -6,7 +6,7 @@
 /*   By: skully <skully@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 12:37:25 by mdakni            #+#    #+#             */
-/*   Updated: 2025/05/16 23:00:40 by skully           ###   ########.fr       */
+/*   Updated: 2025/05/16 23:11:39 by skully           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,6 @@ void syntax_error(t_token type, t_input *list, t_input *iter)
         else
             printf("Syntax Error near unexpected Token '%s' \n", iter->next->value);
     }
-    if(type == TOKEN_AND)
-        printf("Syntax Error near unexpected Token '&&' \n");
-    if(type == TOKEN_OR)
-        printf("Syntax Error near unexpected Token '||' \n");
     ft_lstfree(list);
     exit(EXIT_FAILURE);
 }
@@ -49,13 +45,21 @@ void filter(t_input *list)
         if(list->type == TOKEN_PIPE && list->next->type == TOKEN_PIPE)
             return(syntax_error(TOKEN_PIPE, list, iter));
         if((iter->type == TOKEN_R_RED || iter->type == TOKEN_L_RED) && !(iter->next->value))
-            return(printf("3 : %d\n", iter->index) ,syntax_error(iter->type, list, iter));
+            return(printf("3 : ") ,syntax_error(iter->type, list, iter));
         if((iter->type == TOKEN_R_APP || iter->type == TOKEN_L_APP) && !(iter->next->value))
-            return(printf("4 : %d\n", iter->index) ,syntax_error(iter->type, list, iter));
+            return(printf("4 : ") ,syntax_error(iter->type, list, iter));
         if(iter->type == TOKEN_PIPE && (!(iter->prev) || !(iter->next->value)))
-            return(printf("1 : %d\n", iter->index) ,syntax_error(iter->type, list, iter));
+            return(printf("1 : ") ,syntax_error(iter->type, list, iter));
         if(iter->category == TOKEN_RED_APP && iter->next->category != TOKEN_WORD)
-            return(printf("5 : %d\n", iter->index) ,syntax_error(iter->type, list, iter));
+            return(printf("5 : ") ,syntax_error(iter->type, list, iter));
         iter = iter->next;
+    }
+    if(list->quotes != 0)
+    {
+        if(list->quotes == 1)
+            printf("Quote Error found open \"'\"\n");
+        else if(list->quotes == 2)
+            printf("Quote Error found open '\"'\n");
+        return(ft_lstfree(list), exit(EXIT_FAILURE));
     }
 }
